c9355d79c73904970ca6679ee29f01800957d564
Steve H <steve.high@gmail.com>
2014-06-02 12:39:35 -0400

    Update viewmodel.js
    added check for observable

--- release/widgets/viewmodel.js
+++ release/widgets/viewmodel.js
@@ -66,8 +66,8 @@ define(['durandal/app', 'knockout', 'jquery'], function (app, ko, $) {
 			if (!column.property) {
 				return '';
 			}
-			if ('function' === typeof(column.property)) {
-				return column.property(row);
+			if ('function' === typeof (column.property) && !ko.isObservable(column.property)) {
+			    return column.property(row);
 			}
 			return ko.unwrap(row[column.property]);
 		};
